9cf5be7c2e5d2c02e1e74bb2f111bc8e1483ab51
Mio Engel <mioengel@gmail.com>
2019-02-17 16:07:57 +0100

    Adds logging

--- src/main/java/com/valhallagame/featserviceserver/FeatApp.java
+++ src/main/java/com/valhallagame/featserviceserver/FeatApp.java
@@ -1,14 +1,18 @@
 package com.valhallagame.featserviceserver;
 
 import com.valhallagame.common.Properties;
+import com.valhallagame.common.filter.ServiceRequestFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import com.valhallagame.common.DefaultServicePortMappings;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class FeatApp {
@@ -23,5 +27,23 @@ public class FeatApp {
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 		return (container -> container.setPort(DefaultServicePortMappings.FEAT_SERVICE_PORT));
+	}
+
+	@Bean
+	public FilterRegistrationBean serviceRequestFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(getServiceRequestFilter());
+		registration.addUrlPatterns(
+				"/*",
+				"/**"
+		);
+		registration.setName("serviceRequestFilter");
+		registration.setOrder(1);
+		return registration;
+	}
+
+	@Bean(name = "serviceRequestFilter")
+	public Filter getServiceRequestFilter() {
+		return new ServiceRequestFilter();
 	}
 }
--- src/main/java/com/valhallagame/featserviceserver/controller/FeatController.java
+++ src/main/java/com/valhallagame/featserviceserver/controller/FeatController.java
@@ -1,22 +1,5 @@
 package com.valhallagame.featserviceserver.controller;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.characterserviceclient.CharacterServiceClient;
 import com.valhallagame.characterserviceclient.model.CharacterData;
@@ -29,10 +12,28 @@ import com.valhallagame.featserviceclient.message.GetFeatsParameter;
 import com.valhallagame.featserviceclient.message.RemoveFeatParameter;
 import com.valhallagame.featserviceserver.model.Feat;
 import com.valhallagame.featserviceserver.service.FeatService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path = "/v1/feat")
 public class FeatController {
+	private static final Logger logger = LoggerFactory.getLogger(FeatController.class);
 
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
@@ -46,6 +47,7 @@ public class FeatController {
 	@RequestMapping(path = "/get-feats", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<JsonNode> getFeats(@Valid @RequestBody GetFeatsParameter input) {
+		logger.info("Get Feats called with {}", input);
 		List<Feat> feats = featService.getFeats(input.getCharacterName().toLowerCase());
 		List<String> items = feats.stream().map(Feat::getName).collect(Collectors.toList());
 		return JS.message(HttpStatus.OK, items);
@@ -54,6 +56,7 @@ public class FeatController {
 	@RequestMapping(path = "/add-feat", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<JsonNode> addFeat(@Valid @RequestBody AddFeatParameter input) {
+		logger.info("Add Feat called with {}", input);
 		String characterName = input.getCharacterName().toLowerCase();
 
 		// Duplicate protection
@@ -70,7 +73,7 @@ public class FeatController {
 	@RequestMapping(path = "/remove-feat", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<JsonNode> removeFeat(@Valid @RequestBody RemoveFeatParameter input) throws IOException {
-		
+		logger.info("Remove Feat called with {}", input);
 		RestResponse<CharacterData> characterResp = characterServiceClient.getCharacter(input.getCharacterName());
 		Optional<CharacterData> characterOpt = characterResp.get();
 		if(!characterOpt.isPresent()) {
@@ -86,6 +89,7 @@ public class FeatController {
 					new NotificationMessage(character.getOwnerUsername(), "feat item removed"));
 			return JS.message(HttpStatus.OK, "Feat item removed");
 		} else {
+
 			return JS.message(HttpStatus.NOT_FOUND, "No feat found");
 		}
 	}
