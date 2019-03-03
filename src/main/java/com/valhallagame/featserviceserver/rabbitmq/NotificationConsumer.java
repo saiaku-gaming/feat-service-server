4a762c3f2c9be4cb63ba6765524bfc5dc4ce65d4
Mio Engel <mioengel@gmail.com>
2019-03-03 14:46:06 +0100

    Adds more logging mdc

--- src/main/java/com/valhallagame/featserviceserver/rabbitmq/NotificationConsumer.java
+++ src/main/java/com/valhallagame/featserviceserver/rabbitmq/NotificationConsumer.java
@@ -1,62 +1,100 @@
 package com.valhallagame.featserviceserver.rabbitmq;
 
-import java.util.List;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.valhallagame.common.rabbitmq.NotificationMessage;
 import com.valhallagame.featserviceserver.model.Feat;
 import com.valhallagame.featserviceserver.service.FeatService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class NotificationConsumer {
 
-	Logger logger = LoggerFactory.getLogger(NotificationConsumer.class);
+	private static final Logger logger = LoggerFactory.getLogger(NotificationConsumer.class);
 
 	@Autowired
-	FeatService featService;
+	private FeatService featService;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	@RabbitListener(queues = { "#{featCharacterDeleteQueue.name}" })
 	public void receiveCharacterDelete(NotificationMessage message) {
-		String characterName = (String) message.getData().get("characterName");
-		List<Feat> feats = featService.getFeats(characterName);
-		for (Feat feat : feats) {
-			featService.deleteFeat(feat);
+		MDC.put("service_name", appName);
+		MDC.put("request_id", UUID.randomUUID().toString());
+
+		logger.info("Received character delete notification with message: {}", message);
+
+		try {
+			String characterName = (String) message.getData().get("characterName");
+			List<Feat> feats = featService.getFeats(characterName);
+			for (Feat feat : feats) {
+				featService.deleteFeat(feat);
+			}
+		} finally {
+			MDC.clear();
 		}
 	}
 
 	@RabbitListener(queues = { "#{featStatisticsIntCounterQueue.name}" })
 	public void receiveStatisticsIntCounter(NotificationMessage message) {
-		logger.debug("Got int counter " + message);
-		Map<String, Object> data = message.getData();
-		String characterName = (String) data.get("characterName");
-		String key = (String) data.get("key");
-		int count = (Integer) data.get("count");
-		featService.parseIntCounterData(characterName, key, count);
+		MDC.put("service_name", appName);
+		MDC.put("request_id", UUID.randomUUID().toString());
+
+		logger.info("Got int counter " + message);
+
+		try {
+			Map<String, Object> data = message.getData();
+			String characterName = (String) data.get("characterName");
+			String key = (String) data.get("key");
+			int count = (Integer) data.get("count");
+			featService.parseIntCounterData(characterName, key, count);
+		} finally {
+			MDC.clear();
+		}
 	}
 
 	@RabbitListener(queues = { "#{featStatisticsLowTimerQueue.name}" })
 	public void receiveStatisticsLowTimer(NotificationMessage message) {
-		logger.debug("got low timer " + message);
-		Map<String, Object> data = message.getData();
-		String characterName = (String) data.get("characterName");
-		String key = (String) data.get("key");
-		double timer = (Double) data.get("timer");
-		featService.parseLowTimerData(characterName, key, timer);
+		MDC.put("service_name", appName);
+		MDC.put("request_id", UUID.randomUUID().toString());
+
+		logger.info("got low timer " + message);
+
+		try {
+			Map<String, Object> data = message.getData();
+			String characterName = (String) data.get("characterName");
+			String key = (String) data.get("key");
+			double timer = (Double) data.get("timer");
+			featService.parseLowTimerData(characterName, key, timer);
+		} finally {
+			MDC.clear();
+		}
 	}
 
 	@RabbitListener(queues = { "#{featStatisticsHighTimerQueue.name}" })
 	public void receiveStatisticsHighTimer(NotificationMessage message) {
-		logger.debug("Got high timer " + message);
-		Map<String, Object> data = message.getData();
-		String characterName = (String) data.get("characterName");
-		String key = (String) data.get("key");
-		double timer = (Double) data.get("timer");
-		featService.parseHighTimerData(characterName, key, timer);
+		MDC.put("service_name", appName);
+		MDC.put("request_id", UUID.randomUUID().toString());
+
+		logger.info("Got high timer " + message);
+
+		try {
+			Map<String, Object> data = message.getData();
+			String characterName = (String) data.get("characterName");
+			String key = (String) data.get("key");
+			double timer = (Double) data.get("timer");
+			featService.parseHighTimerData(characterName, key, timer);
+		} finally {
+			MDC.clear();
+		}
 	}
 }
